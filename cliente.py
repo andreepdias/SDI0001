74d60d92670a5728677063daac87771b86929ada
Wilton <wilton.loch@hotmail.com.br>
2018-06-08 13:41:59 -0300

    cliente faz todos os requests pro server

--- cliente.py
+++ cliente.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import socketserver
+import os
 from sys import argv
 
 if len(argv) < 2:
@@ -10,23 +11,75 @@ if len(argv) < 2:
 SERVERIP, SERVERPORT = "localhost", int(argv[1])
 
 nome = ""
-numero_indices = 0
+msg_atual = ""
+mensagens_conhecidas = 0
+id_mensagem = 0
+mensagens = [""] * 100
 
-def solicitaNumeroIndices():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        sock.connect((SERVERIP, SERVERPORT))
-        envio = ";0" + ";" + "\n";
-        sock.sendall(bytes(envio, "utf-8"))
-        recebido = str(sock.recv(1024), "utf-8")
-    finally:
-        sock.close()
-    return int(recebido)
+def mandaNovaMensagem(nome, texto):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		sock.connect((SERVERIP, SERVERPORT))
+		envio = ";0;" + "<" + nome + ">:" + texto
+		sock.sendall(bytes(envio, "utf-8"))
+		recebido = str(sock.recv(1024), "utf-8")
+	finally:
+		sock.close()
+	return int(recebido)
+
+def requisicaoTotalMensagens():
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		sock.connect((SERVERIP, SERVERPORT))
+		envio = ";3;"
+		sock.sendall(bytes(envio, "utf-8"))
+		recebido = str(sock.recv(1024), "utf-8")
+	finally:
+		sock.close()
+	return int(recebido)
+
+def requestPossuidoresMensagem(indice):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		sock.connect((SERVERIP, SERVERPORT))
+		envio = ";1;" + str(indice)
+		sock.sendall(bytes(envio, "utf-8"))
+		recebido = str(sock.recv(1024), "utf-8")
+	finally:
+		sock.close()
+	return recebido
+
+def requestMensagem(IPDESTINO, indice):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		sock.connect((IPDESTINO, 8888))
+		envio = ";1;" + str(indice)
+		sock.sendall(bytes(envio, "utf-8"))
+		recebido = str(sock.recv(1024), "utf-8")
+	finally:
+		sock.close()
+	return recebido
+
 
 if __name__ == "__main__":
-    print ("Digite seu nome:")
-    nome = input()
-
-    numero_indices = solicitaNumeroIndices()
-
-    print (numero_indices)
+	print("Digite seu nome:")
+	nome = input()
+	escolha = ""
+	while escolha != "/sair":
+		# os.system("clear")
+		if(id_mensagem > mensagens_conhecidas):
+			print("***Você possui novas mensagens não lidas***")
+		print("1 - mandar mensagem\n2 - receber mensagens\nEscolha uma opção: ")
+		escolha = input()
+		if(escolha == "1"):
+			msg_atual = input()
+			id_mensagem = mandaNovaMensagem(nome, msg_atual)
+			if(id_mensagem == mensagens_conhecidas):
+				mensagens_conhecidas += 1
+			mensagens[id_mensagem] = msg_atual
+		if(escolha == "2"):
+			total_mensagens = requisicaoTotalMensagens()
+			for i in range(mensagens_conhecidas, total_mensagens):
+				if(mensagens[i] == ""):
+					possuidores = split(requestPossuidoresMensagem(i), ";")
+					mensagens[i] = requestMensagem(possuidores[0], i)
--- servidor.py
+++ servidor.py
@@ -7,7 +7,7 @@ if len(argv) < 2:
 
 SERVERIP, SERVERPORT = "localhost", int(argv[1])
 
-numero_indices = 0
+possuidores = []
 
 class ConexaoTCP(socketserver.BaseRequestHandler):
 
@@ -17,20 +17,31 @@ class ConexaoTCP(socketserver.BaseRequestHandler):
 
 		dados = str(self.data)
 		dados = dados.split(";")
-		print(dados[1])
+		dados[2] = dados[2][0:-1]
+		print(dados)
 
 		envio = ""
 
 		if(dados[1] == "0"):
-			envio = str(numero_indices)
-
+			#o cliente mandou uma mensagem nova, adiciona-o como possuidor da mesma na lista de possuidores
+			possuidores.append(str(self.client_address[0]))
+			envio = str(len(possuidores) - 1)
+		if(dados[1] == "1"):
+			#o cliente quer saber quem possui a mensagem com o índice contido em dados[2]
+			envio = str(possuidores[int(dados[2])])
+		if(dados[1] == "2"):
+			# o cliente está informando que possui a mensagem com o índice contido em dados[2]
+			possuidores[int(dados[2])] += ";" + str(self.client_address[0])
+		if(dados[1] == "3"):
+			 # o cliente está requisitando o total de mensagens
+			envio = str(len(possuidores))
 		self.request.sendall(bytes(envio, "utf-8"))
 
 if __name__ == "__main__":
-    try:
-        server = socketserver.TCPServer((SERVERIP, SERVERPORT), ConexaoTCP)
-        server.serve_forever()
-    except KeyboardInterrupt:
-    	print("Stopping server")
-    finally:
-    	server.server_close()
+	try:
+		server = socketserver.TCPServer((SERVERIP, SERVERPORT), ConexaoTCP)
+		server.serve_forever()
+	except KeyboardInterrupt:
+		print("Stopping server")
+	finally:
+		server.server_close()
